f58d63c2ff35a07b29a3c912dfb68c6c1aac6ab2
AgrawalTrisha <agrawaltrisha@gmail.com>
2020-07-10 02:03:03 -0500

    finished

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.green,
         backgroundColor: Color(0xFFf2f4f3),
